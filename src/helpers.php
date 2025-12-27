951d4710f564786a86ea5be6fb2b183f47ac4fb0
Wakar Mujtaba <wakarmujtaba2@gmail.com>
2025-12-27 20:50:10 +0400

    Fix PSR-4 folder structure

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,6 @@ use Wakar\DateFormatHelper\Helpers\DateFormatHelper;
 if (!function_exists('format_date')) {
     function format_date($date, $format = 'd-m-Y', $returnOriginalOnFailure = true)
     {
-        return DateFormatHelper::format($date, $format, $returnOriginalOnFailure);
+        return DateFormatHelper::format_date($date, $format, $returnOriginalOnFailure);
     }
 }
